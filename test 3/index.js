7d5305bf8edfb45a1b6c6233f5fbaa9e049a939f
Alireza <alirezasaadat056@gmail.com>
2024-08-02 07:48:14 -0700

    update

--- test 3/index.js
+++ test 3/index.js
@@ -4,10 +4,24 @@ import { OrbitControls } from "jsm/controls/OrbitControls.js";
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+class Sphere extends THREE.Mesh{
+    constructor(radius , color = "#00ff00"){
+        super(
+            new THREE.SphereGeometry(radius , 32 , 32),
+            new THREE.MeshStandardMaterial({color})
+        );
+        this.radius = radius;
+
+        this.top = this.position.y + this.radius;
+        this.bottom = this.position.y - this.radius;
+
+    }
+}
+
 // Renerer
 const renderer = new THREE.WebGLRenderer({ antialias : true});
 renderer.setSize(width , height);
-
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // Scene
@@ -22,22 +36,22 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 
-const planeMesh = new THREE.Mesh(
-    new THREE.IcosahedronGeometry(2 , 4),
-    new THREE.MeshStandardMaterial({ color: 0x00ff00 , flatShading: true })
-);
-scene.add(planeMesh);
+const player = new Sphere(1 , 0x00ff00);
+player.castShadow = true;
+scene.add(player);
 
 const groundMesh = new THREE.Mesh(
-    new THREE.BoxGeometry(20 , 1 , 20),
+    new THREE.BoxGeometry(30 , 1 , 30),
     new THREE.MeshStandardMaterial({color: 0xffffff})
 );
+groundMesh.receiveShadow = true;
 groundMesh.position.y = -2;
 scene.add(groundMesh);
 
 // Light
 const light = new THREE.DirectionalLight(0xffffff , 1);
 light.position.set(5 , 5 , 5);
+light.castShadow = true;
 scene.add(light);
 
 const light2 = new THREE.HemisphereLight( 0xffffff , 0x000000 , 1);
